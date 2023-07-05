8d11d88ea53ba6d91d6b9603a216f2b1ce5d113b
Kanggyeonggu <kku1031@naver.com>
2023-07-06 02:27:55 +0900

    사용자 권한 인증{JWT : 사용자 인증 및 식별에 사용되는 토큰(사용자 정보를 포함)} 토큰 발급, 생성

--- src/main/java/zerobase/dividend/security/TokenProvider.java
+++ src/main/java/zerobase/dividend/security/TokenProvider.java
@@ -1,7 +1,75 @@
 package zerobase.dividend.security;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
 
+    //토큰 만료 시간
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1hour ms, 초, 분 ex)5시간 1000 * 60 * 60 * 5
 
+    //회원가입 권한 역할
+    private static final String KEY_ROLES = "roles";
 
+    //토큰값 ubuntu cmd에서 echo 'zerobase-spring-boot-dividend-project-tutorial-jwt-secret-key' | base64로 생성
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    /**
+     * 토큰 생성(발급)
+     * @param username
+     * @param roles
+     * @return
+     */
+    //토큰값 문자열 : Auth(로그인한 사용자 이름, 역할) 참조
+    public String generateToken(String username, List<String> roles) {
+        //사용자 권한 정보 저장
+        Claims claims = Jwts.claims().setSubject(username);
+        //claims데이터는 Key, Value  형태로 저장되어야함.
+        claims.put(KEY_ROLES, roles);
+
+        //토큰 생성시간 및 만료 시간 설정
+        var nowDate = new Date();
+        var expiredDate = new Date(nowDate.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(nowDate)   //토큰 생성 시간
+                .setExpiration(expiredDate) //토큰 만료 시간
+                .signWith(SignatureAlgorithm.ES512, secretKey) //사용할 암호화 알고리즘, 비밀키
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        //generateToken메소드에서 넣어준 setSubject(username)값 반환
+        return parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) return false;
+        var claims = parseClaims(token);
+        //토큰의 만료시간이 현재의 시간인지 아닌지로 비교.
+        return !claims.getExpiration().before(new Date());
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
 }
